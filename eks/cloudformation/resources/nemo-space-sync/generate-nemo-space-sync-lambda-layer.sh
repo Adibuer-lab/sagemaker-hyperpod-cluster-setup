6114fc190b08f0fef16add6cce4bbb949dff18aa
Benedict Nartey-Tokoli <benedicttokoli@gmail.com>
2025-12-19 18:58:17 +0000

    chore(eks): Rebuild NeMo space sync Lambda function and layer artifacts
    - Update nemo-space-sync-lambda-function.zip with latest function code
    - Add nemo-space-sync-lambda-layer.zip to primary artifacts directory
    - Fix generate-nemo-space-sync-lambda-layer.sh to output to correct artifacts path
    - Update resources2 artifacts with rebuilt layer package
    - Consolidate Lambda layer artifact generation to primary resources directory

--- eks/cloudformation/resources/nemo-space-sync/generate-nemo-space-sync-lambda-layer.sh
+++ eks/cloudformation/resources/nemo-space-sync/generate-nemo-space-sync-lambda-layer.sh
@@ -6,7 +6,7 @@ docker build $DOCKER_NETWORK -t lambda-layer-builder .
 
 # Run the container and copy the zip file
 docker run --rm \
-  -v $(pwd)/../../resources2/artifacts:/layer/artifacts \
+  -v $(pwd)/../../resources/artifacts:/layer/artifacts \
   lambda-layer-builder \
   bash -c "chmod +x build-layer.sh && ./build-layer.sh && cp nemo-space-sync-lambda-layer.zip /layer/artifacts/"
 
